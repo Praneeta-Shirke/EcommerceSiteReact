dd0f75d27f8de7d9100743bd3011a2d8e7508bd1
Praneeta-Shirke <praneetashirke30@gmail.com>
2025-04-04 23:41:03 +0530

    In Header added logo, icons and a search bar

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import '../header/header.css';
 
+import Logo from '../../assets/images/logo.png'
+import SearchIcon from '@mui/icons-material/Search';
+
 const Header =()=>{
     return(
         <>
+        <header>
+            <div className='container-fluid'>
+                <div className="row">
+                    <div className='col-sm-2'>
+                        <img src={Logo} alt="Logo"  style={{ maxWidth: '100%', maxHeight: '70%', objectFit: 'contain' }} />
+                    </div>
+                    {/*Header Search starts in this div*/}
+                    <div className='col-sm-5'>
+                        <div className='headerSearch d-flex align-items-center'>
+                            <div className='selectDrop cursor'>All Categories</div>
+
+                            <div className="search">
+                                <input type='text' placeholder="Search for items.."/>
+                                <SearchIcon className="searchIcon cursor"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
         </>
     )
 
