ad47f6fac95e2f60747e00b4b844626ef60e4923
Praneeta-Shirke <praneetashirke30@gmail.com>
2025-04-17 15:02:56 +0530

    FallBack Image added

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./ProductCard.css";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import fallbackImage from "../assets/images/FallbackImage.png"; 
 
 const ProductCard = ({ images, title, price }) => {
-  
+  const handleImageError = (e) => {
+    e.target.onerror = null;
+    e.target.src = fallbackImage;
+  };
+
   return (
     <div className="product-card">
-      <img src={images?.[0]} alt={title} className="product-image" />
+      <img
+        src={images?.[0] || fallbackImage}
+        alt={title || "Product Image"}
+        className="product-image"
+        onError={handleImageError}
+      />
       <h5 className="product-title">{title}</h5>
       <p className="product-price">₹{(price * 100).toFixed()}</p>
       <button className="add-to-cart-btn">
